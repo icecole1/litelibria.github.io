8f78f7db79dbc0fe00aaa12ba79ea4a916000717
Rozenrod <Rozenrod320@gmail.com>
2022-02-09 22:52:26 +0200

    Обновление manifest и т.д.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+// Проверка того, что наш браузер поддерживает Service Worker API.
+if ('serviceWorker' in navigator) {
+    // Весь код регистрации у нас асинхронный.
+    navigator.serviceWorker.register('./sw.js')
+      .then(() => navigator.serviceWorker.ready.then((worker) => {
+        worker.sync.register('syncdata');
+      }))
+      .catch((err) => console.log(err));
+}
+
+
+
 // Загрузка тайтлов Анилибрии в кеш браузера
 function load_api_cash(index_article, n) {
   var api_cash = localStorage.getItem('my_api_cash');
