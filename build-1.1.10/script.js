4b5adc188f49db9c64db94968169c6cb3889ca33
Rozenrod <Rozenrod320@gmail.com>
2022-04-09 20:11:39 +0300

    Peterugiusu-1.1.10

--- build-1.1.10/script.js
+++ build-1.1.10/script.js
@@ -1,12 +1,12 @@
-// // Проверка того, что наш браузер поддерживает Service Worker API.
-//  if ('serviceWorker' in navigator) {
-//      // Весь код регистрации у нас асинхронный.
-//      navigator.serviceWorker.register('./sw.js')
-//        .then(() => navigator.serviceWorker.ready.then((worker) => {
-//          worker.sync.register('syncdata');
-//        }))
-//        .catch((err) => console.log(err));
-//  }
+// Проверка того, что наш браузер поддерживает Service Worker API.
+ if ('serviceWorker' in navigator) {
+     // Весь код регистрации у нас асинхронный.
+     navigator.serviceWorker.register('./sw.js')
+       .then(() => navigator.serviceWorker.ready.then((worker) => {
+         worker.sync.register('syncdata');
+       }))
+       .catch((err) => console.log(err));
+ }
 
 function load_api_home(n) {
 	document.getElementById("load_anim").style.display = "";
