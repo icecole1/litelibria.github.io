42557776a4611937a66e278262a12af6825f3800
Rozenrod <Rozenrod320@gmail.com>
2022-04-13 12:57:00 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ let config = {
   'g_apiKey': 'AIzaSyDnCKI8zpxJ5Iwpa7xpXx1jkKZY0Gr9yqo',
   'g_clientId': '116898535578-na8fsq2p1igt77ve615sa444ur2l1bvv.apps.googleusercontent.com',
   'domains': 'litelibria.github.io',
-  'titels_api':'https://api.anilibria.tv/v2/'
+  'titels_api':'http://api.sdev.xyz/v2/'
 }
